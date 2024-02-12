56541011b4ae6eb305c10e47a879f5b385cfacf8
Adrian Dole <adrian@dole.tech>
2024-02-12 13:53:17 -0800

    Update checkouts to mine

--- .evergreen/scripts/run-mongodb-aws-ecs-test.sh
+++ .evergreen/scripts/run-mongodb-aws-ecs-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # run-mongodb-aws-ecs-test.sh is intended to run on a remote ECS host.
-# For a description of ECS tests, see: https://github.com/mongodb-labs/drivers-evergreen-tools/tree/b01493d57e6716cb6385afaa4dc06330e4f33e01/.evergreen/auth_aws#ecs-test-process
+# For a description of ECS tests, see: https://github.com/adriandole/drivers-evergreen-tools/tree/b01493d57e6716cb6385afaa4dc06330e4f33e01/.evergreen/auth_aws#ecs-test-process
 
 # ECS tests have paths /root/src
 
